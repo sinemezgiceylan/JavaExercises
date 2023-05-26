052b3cd74c97d546987056eaa30d4fab7f2091f0
Sinem Ezgi Ceylan <sinemezgic0@gmail.com>
2023-05-26 18:02:15 +0300

    first commit

---
+++ src/SortingNumbers.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class SortingNumbers {
+    public static void main(String[] args) {
+
+        int number1, number2, number3;
+
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter first number: ");
+        number1 = input.nextInt();
+        System.out.print("Enter second number: ");
+        number2 = input.nextInt();
+        System.out.print("Enter third number: ");
+        number3 = input.nextInt();
+
+        if ((number1 < number2) && (number1 < number3)) {
+            if (number2 < number3) {
+                System.out.println(number1 + " < " + number2 + " < " + number3);
+            } else {
+                System.out.println(number1 + " < " + number3 + " < " + number2);
+            }
+        } else if ((number2 < number1) && (number2 < number3)) {
+            if (number1 < number3) {
+                System.out.println(number2 + " < " + number1 + " < " + number3);
+            } else {
+                System.out.println(number2 + " < " + number3 + " < " + number1);
+            }
+        }else {
+            if (number1 < number2) {
+                System.out.println(number3 + " < " + number1 + " < " + number2);
+            } else {
+                System.out.println(number3 + " < " + number2 + " < " + number1);
+            }
+        }
+    }
+}
